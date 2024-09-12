f90098190bffdf95e8d89099a8da9a539d85ac51
Windy Arya <windyarya4@gmail.com>
2024-09-12 23:41:24 +0700

    [Fix] update dockerfile

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://127.0.0.1:5173", "http://localhost:5173", "http://ec2-"},
+		AllowOrigins: []string{"http://127.0.0.1:5173", "http://localhost:5173", "http://ec2-13-215-179-160.ap-southeast-1.compute.amazonaws.com", "http://ec2-13-215-179-160.ap-southeast-1.compute.amazonaws.com:80", "http://ec2-13-215-179-160.ap-southeast-1.compute.amazonaws.com:8080", "http://ec2-13-215-179-160.ap-southeast-1.compute.amazonaws.com:443"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))
